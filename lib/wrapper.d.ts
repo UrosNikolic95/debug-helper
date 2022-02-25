1765373d4cc4ea4b9e81fe2bb14e3a314cae81dd
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-02-25 23:02:31 +0100

    setup jest, compiling

---
+++ lib/wrapper.d.ts
@@ -0,0 +1,3 @@
+export declare function GetAllFunctionNames(obj: any): string[];
+export declare function WrapSingleFunction(func: Function): Function;
+export declare function WrapFunctions(obj: any): void;
