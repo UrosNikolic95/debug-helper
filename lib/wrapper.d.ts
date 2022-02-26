7b9b443dd81ede85008d1c9d89d80dc41a8a2a91
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-02-26 19:54:58 +0100

    remove compiled

--- lib/wrapper.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function GetAllFunctionNames(obj: any): string[];
-export declare function WrapSingleFunction(func: Function): Function;
-export declare function WrapFunctions(obj: any): void;
